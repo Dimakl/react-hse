988a44d92fc076e722e8106dad430617e7970800
Dmitrii Kocheshkov <dimakid123@gmail.com>
2020-10-21 16:25:24 +0300

    minor changes

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -4,6 +4,12 @@ import './css/Task.css'
 // Task is Class Component, instead of Functional Component, because of having "completed" state and as a growndwork for future possibility of editing fields inside of each Task.
 class Task extends React.Component {
 
+    taskButtonColors = {
+      green: "#196F3D",
+      red: "#922B21"
+    } 
+
+    
     constructor(props) {
       super(props)
       this.state = {
@@ -12,10 +18,7 @@ class Task extends React.Component {
       this.buttonTextStyle =  {color: this.state.completed ? this.taskButtonColors.green : this.taskButtonColors.red, fontWeight: "bold"}
     }
 
-    taskButtonColors = {
-      green: "#196F3D",
-      red: "#922B21"
-    }
+    
   
     onButtonClick = () => {
       console.log(`Task ${this.state.id} completed status = ${this.state.completed}`)
